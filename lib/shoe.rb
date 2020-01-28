25eeff82767d75236c78b30fe77fd7c61fbc0389
Craig Storer <craigstorer@gmail.com>
2020-01-28 17:59:27 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,7 +7,11 @@ class Shoe
   end  
   
 
+<<<<<<< HEAD
   def cobble
+=======
+  def cobble(condition)
+>>>>>>> 0760af2f753760eede576047c7c49988532d0e6c
        puts  "Your shoe is as good as new!"
   @condition = "new"
  
